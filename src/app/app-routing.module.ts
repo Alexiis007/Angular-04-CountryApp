31fcd249e7bf795e7f7a70edd36c27fec93d047b
Alexiis007 <christianalexis0918@gmail.com>
2024-04-10 11:49:00 -0600

    Comentarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomePageComponent } from './shared/pages/home-page/home-page.component'
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactPagesComponent } from './shared/pages/contact-pages/contact-pages.component';
 import { CountryPageComponent } from './countries/pages/country-page/country-page.component';
-
 const routes: Routes = [
+  //Las rutas del share se agregan directo (usualmente no pasan de 3 rutas)
  {
   path:'home',
   component: HomePageComponent
@@ -18,10 +18,16 @@ const routes: Routes = [
   path:'contact',
   component: ContactPagesComponent
  },
+ //Las rutas de los modulos se encapsulan por modulo para no agregar todas las pages que tiene
+ //el modulo simplemente se enlaza hacia el modulo, ya en el modulo tendra su propio routing.
+ //shared cuenta como modulo pero como no pasan de 3 pages pues lo agregamos directo como se comento
+ //mas arriva
  {
   path:'countries',
   loadChildren:()=>import('./countries/countries.module').then( m => m.CountriesModule )
  },
+ //La definicion del la pagina o ruta principal o preedefinida se define con path"**" y con un redirectTo
+ //Que serua un path definido anteriormente
  {
   path:'**',
   redirectTo:'countries'
@@ -29,7 +35,15 @@ const routes: Routes = [
 ];
 
 @NgModule({
+  //el app routing es ForRoot, en los modulos de mas es ForChild
+  //Tambien Notese que le mandamos las rutas (routes)
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+//Para que el routing funcione se deben importar los modulos que se usaran en el app.module
+//Esto solo aplica si es que se hara el definido del component directo
+//En caso de que se hara referencia a un modulo como ruta ahi no se debe importar ya que la misma
+//linea de codigo la importa
--- src/app/countries/countries.routing.module.ts
+++ src/app/countries/countries.routing.module.ts
@@ -4,8 +4,9 @@ import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
-
+//Archivo de routing Hija
 const routes : Routes=[
+  //Las rutas se definen por su path y su componente
   {
     path:'byCapital',
     component: ByCapitalPageComponent
@@ -18,10 +19,15 @@ const routes : Routes=[
     path:'byRegion',
     component: ByRegionPageComponent
   },
+  //En este caso esta ruta cuenta con un id el cual cambia la direccion de la ruta
+  //dependiendo del valor que se le mande
   {
     path:'by/:id',
     component: CountryPageComponent
   },
+  //En el config routing padre ya se habia definido una ruta defecto la cual mandaba siempre al modulo
+  //countries que seria este modulo, una vez entra al modulo carga si es que tiene sus rutas
+  //en este caso carga la defecto al iniciar:
   {
    path:'**',
    redirectTo:'byCapital'
@@ -30,6 +36,7 @@ const routes : Routes=[
 
 @NgModule({
   imports: [
+    //El for child que indica que es una ruta hija
     RouterModule.forChild(routes)
   ],
   exports: [
@@ -37,3 +44,5 @@ const routes : Routes=[
   ],
 })
 export class CountriesRoutingModule { }
+
+//Este archivo debe estar importado en du archivo de modulos
